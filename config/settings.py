fb546771c20dcf434cc1a9b816b225267aa83787
Khidirov Ali <alihidirov285@gmail.com>
2025-11-01 21:26:24 +0600

    зщыекуыйд пф гдфтвш

--- config/settings.py
+++ config/settings.py
@@ -76,23 +76,14 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': "lms_db",
-#         'USER': "lms_admin",
-#         'PASSWORD': 'root',
-#         'HOST': '127.0.0.1',
-#         'PORT': '5432',
-#     }
-# }
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': "univercity_db",
-        'USER': "univercity_admin",
-        'PASSWORD': 'root',
-        'HOST': '127.0.0.1',
+        'NAME': 'university_db',
+        'USER': 'university_admin',
+        'PASSWORD': 'qwerty',
+        'HOST':'127.0.0.1',
         'PORT': '5432',
     }
 }
